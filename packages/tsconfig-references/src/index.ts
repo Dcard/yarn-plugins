93c80b5d74f252f653720ff720cb8b9f31349273
Tommy Chen <tommy351@gmail.com>
2020-02-27 15:39:57 +0800

    feat: Add tsconfig-references plugin

---
+++ packages/tsconfig-references/src/index.ts
@@ -0,0 +1,110 @@
+import { Plugin, Workspace, Descriptor } from '@yarnpkg/core';
+import { Hooks, suggestUtils } from '@yarnpkg/plugin-essentials';
+import { ppath, toFilename, xfs, PortablePath } from '@yarnpkg/fslib';
+import detectIndent from 'detect-indent';
+
+function getTsConfigPath(workspace: Workspace) {
+  return ppath.join(workspace.cwd, toFilename('tsconfig.json'));
+}
+
+interface TsReference {
+  path: string;
+}
+
+interface TsConfig {
+  indent: string;
+  references: TsReference[];
+  [key: string]: any;
+}
+
+async function readTsConfig(workspace: Workspace): Promise<TsConfig> {
+  const path = getTsConfigPath(workspace);
+  const exist = await xfs.existsPromise(path);
+
+  if (!exist) {
+    return {
+      references: [],
+      indent: '  ',
+    };
+  }
+
+  const content = await xfs.readFilePromise(path, 'utf8');
+
+  return {
+    ...JSON.parse(content),
+    indent: detectIndent(content).indent,
+  };
+}
+
+async function writeTsConfig(workspace: Workspace, { indent, ...tsConfig }: TsConfig) {
+  const path = getTsConfigPath(workspace);
+  await xfs.writeFilePromise(path, JSON.stringify(tsConfig, null, indent));
+}
+
+function isLocalPackage(descriptor: Descriptor) {
+  return descriptor.range.startsWith('workspace:');
+}
+
+function uniqTsReference(references: TsReference[]) {
+  const obj: Record<string, TsReference> = {};
+
+  for (const ref of references) {
+    obj[ref.path] = ref;
+  }
+
+  return Object.values(obj);
+}
+
+function getReferencePath(workspace: Workspace, descriptor: Descriptor) {
+  const src = workspace.cwd;
+  const dest = ppath.join(workspace.project.cwd, descriptor.range.substring(10) as PortablePath);
+
+  return ppath.relative(src, dest);
+}
+
+async function afterWorkspaceDependencyAddition(
+  workspace: Workspace,
+  target: suggestUtils.Target,
+  descriptor: Descriptor,
+) {
+  if (!isLocalPackage(descriptor)) {
+    return;
+  }
+
+  const tsConfig = await readTsConfig(workspace);
+
+  await writeTsConfig(workspace, {
+    ...tsConfig,
+    references: uniqTsReference([
+      ...tsConfig.references,
+      { path: getReferencePath(workspace, descriptor) },
+    ]),
+  });
+}
+
+async function afterWorkspaceDependencyRemoval(
+  workspace: Workspace,
+  target: suggestUtils.Target,
+  descriptor: Descriptor,
+) {
+  if (!isLocalPackage(descriptor)) {
+    return;
+  }
+
+  const tsConfig = await readTsConfig(workspace);
+  const refPath = getReferencePath(workspace, descriptor);
+
+  await writeTsConfig(workspace, {
+    ...tsConfig,
+    references: uniqTsReference(tsConfig.references.filter(ref => ref.path !== refPath)),
+  });
+}
+
+const plugin: Plugin<Hooks> = {
+  hooks: {
+    afterWorkspaceDependencyAddition,
+    afterWorkspaceDependencyRemoval,
+  },
+};
+
+export default plugin;
